3d77c5c42d8b0b3ff8684cade19bce52f2856657
Russell Gordon <rgordon@lcs.on.ca>
2024-01-12 08:36:58 -0500

    Initial Commit

---
+++ Home/HomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeApp.swift
+//  Home
+//
+//  Created by Russell Gordon on 2024-01-12.
+//
+
+import SwiftUI
+
+@main
+struct HomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
